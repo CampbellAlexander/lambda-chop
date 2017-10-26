de7eb324d21d5c4d33c4d2c6e04254873b7d0acd
Alexander Campbell <inappropriatelyoptimized@gmail.com>
2017-10-26 16:36:27 -0400

    Added Map for arrays and lists

---
+++ src/LambdaChop/LambdaChop/Extensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaChop
+{
+    public static class Extensions
+    {
+        public static T[] Map<T>(this T[] array, Func<T, T> func)
+        {
+            T[] output = new T[array.Length];
+            for (int i = 0; i < array.Length; i++)
+                output[i] = func.Invoke(array[i]);
+            return output;
+        }
+
+        public static List<T> Map<T>(this List<T> list, Func<T, T> func)
+        {
+            List<T> output = new List<T>(list.Count);
+            for (int i = 0; i < list.Count; i++)
+                output[i] = func.Invoke(list[i]);
+            return output;
+        }
+    }
+}
---
+++ src/LambdaChop/LambdaChopTests/ExtensionsTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LambdaChop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaChop.Tests
+{
+
+    [TestClass()]
+    public class ExtensionsTests
+    {
+        private readonly int[] zeros = new int[5] { 0, 0, 0, 0, 0 };
+        private readonly int[] ones = new int[5] { 1, 1, 1, 1, 1 };
+        private readonly int[] twos = new int[5] { 2, 2, 2, 2, 2 };
+        private int Square(int x) => x * x;
+
+        [TestMethod()]
+        public void Map_SquareAll0s_NewArrayAll0s()
+        {
+            int[] output = zeros.Map(Square);
+
+            foreach (int i in output)
+                if (i != 0)
+                    Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void Map_SquareAll1s_NewArrayAll1s()
+        {
+            int[] output = ones.Map(Square);
+
+            foreach (int i in output)
+                if (i != 1)
+                    Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void Map_SquareAll2s_NewArrayAll4s()
+        {
+            int[] output = twos.Map(Square);
+
+            foreach (int i in output)
+                if (i != 4)
+                    Assert.Fail();
+        }
+    }
+}
